5752cc8a15c36739889a2b13422bfd0f3e0be0a4
mujahidfisher <fishermujahid@gmail.com>
2023-08-01 09:25:40 +0200

    body request

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Body Request
+
+const group = {
+    method: "GET",
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    headers: {
+        "Content-Type": "application/json",
+        "X-RapidAPI-Host": "jsonplaceholder.typicode.com"
+    },
+    body: '{"url":"https://www.theguardian.com/world/2020/oct/24/thousands-join-poland-protests-against-strict-abortion-laws"}'
+}
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+console.log(group);
 
